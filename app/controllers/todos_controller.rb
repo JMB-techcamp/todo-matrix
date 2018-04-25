df91d723a42175152323cc8fc2997cb0dd6ec1cb
JMBereder <jeanmichelbot@gmail.com>
2018-04-25 15:40:26 +0900

    Registration page updated.

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -8,6 +8,15 @@ class TodosController < ApplicationController
   end
 
   def create
+    dead_date = Time.parse(todo_params[:dead_date]).strftime("%Y-%m-%d")
+    dead_time = Time.parse(todo_params[:dead_time]).strftime("%H:%M:%S")
+    dead_line = Time.parse(dead_date +" "+ dead_time).strftime("%F %T")
+    Todo.create(title: todo_params[:title], dead_line: dead_line, detail: todo_params[:detail], user_id: current_user.id)
+  end
+
+  private
+  def todo_params
+    params.permit(:title, :dead_date, :dead_time, :detail)
   end
 
 end

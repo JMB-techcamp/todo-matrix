f6528aaf853e29563356b6131051d56624e6d92d
JMBereder <jeanmichelbot@gmail.com>
2018-04-25 14:55:54 +0900

    Task registration page created.

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -4,4 +4,10 @@ class TodosController < ApplicationController
     @todo = Todo.find(params[:id])
   end
 
+  def new
+  end
+
+  def create
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'users#index'
+
   get  'user/:id'       => 'users#index'
-  get  'todos/:id'      => 'todos#show'
   get  'user/:id/list'  => 'users#list'
+
+  get  'todos/new'      => 'todos#new'
+  get  'todos/:id'      => 'todos#show'
+  post 'todos'          => 'todos#create'
 end

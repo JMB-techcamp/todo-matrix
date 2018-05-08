03d61ab8fbc8dd207c2dd364a40524db67cae71d
JMBereder <jeanmichelbot@gmail.com>
2018-05-08 17:56:40 +0900

    Overall UI improvement

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   def index
     # binding.pry
-    @todos = Todo.where(user_id: params[:id])
+    @todos = Todo.where(user_id: params[:id]).order('importance ASC')
   end
 
 end

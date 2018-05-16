0f46fef1bbc211e4f0849e8564daa81308fe7368
JMBereder <jeanmichelbot@gmail.com>
2018-05-16 12:21:21 +0900

    Fixed error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,26 +5,30 @@ class UsersController < ApplicationController
   def show
     redirect_to new_user_session_path unless user_signed_in?
 
-    @todos_important = []
-    @todos_delegate  = []
-    @todos_decide = []
-    @todos_delete = []
+    # @todos_important = []
+    # @todos_delegate  = []
+    # @todos_decide = []
+    # @todos_delete = []
 
     todos = Todo.where(user_id: current_user.id).order('dead_line ASC')
-
-    if todos.present? then
-      todos_slice = todos.each_slice(todos.length/2+1).to_a
+    if todos.present? and todos.length >= 2 then
+      todos_slice = todos.each_slice(todos.length/2).to_a if todos.length % 2 == 0
+      todos_slice = todos.each_slice(todos.length/2+1).to_a if todos.length % 2 == 1
       # binding.pry
       todos_urgent = todos_slice[0].sort{|a,b| a.importance <=> b.importance}
       todos_noturgent = todos_slice[1].sort{|a,b| a.importance <=> b.importance}
 
-      todos_slice_urg = todos_urgent.each_slice(todos_urgent.length/2+1).to_a
+      todos_slice_urg = todos_urgent.each_slice(todos_urgent.length/2).to_a if todos_urgent.length % 2 == 0
+      todos_slice_urg = todos_urgent.each_slice(todos_urgent.length/2+1).to_a if todos_urgent.length % 2 == 1
       @todos_important = todos_slice_urg[0]
       @todos_delegate  = todos_slice_urg[1]
 
-      todos_slice_noturg = todos_noturgent.each_slice(todos_noturgent.length/2+1).to_a
+      todos_slice_noturg = todos_noturgent.each_slice(todos_noturgent.length/2).to_a if todos_noturgent.length % 2 == 0
+      todos_slice_noturg = todos_noturgent.each_slice(todos_noturgent.length/2+1).to_a if todos_noturgent.length % 2 == 1
       @todos_decide = todos_slice_noturg[0]
       @todos_delete = todos_slice_noturg[1]
+    elsif todos.length == 1 then
+      @todos_important = todos
     end
   end
 

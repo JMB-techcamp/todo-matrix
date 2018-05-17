822338c0c8e1c129a5ba5c6f9b38d6ab58b3e591
JMBereder <jeanmichelbot@gmail.com>
2018-05-17 15:29:41 +0900

    Installed pickadate.js

--- app/assets/javascripts/picker-ui.js
+++ app/assets/javascripts/picker-ui.js
@@ -1,5 +1,10 @@
 $(document).on('turbolinks:load', function(){
-$('#date_box').pickadate({
+$("#date_box").pickadate({
     format: 'yyyy/mm/dd'
 });
+
+$("#time_box").pickatime({
+    format: 'h:i'
+});
+
 });
